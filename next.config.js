dcdb7addc1051908de8ab0d9ea30be196219b6c3
Jesse <jesse@empire.local>
2026-03-24 01:36:27 +0100

    fix: Force rebuild - all files present

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   output: 'standalone',
   images: {
     unoptimized: true
-  }
+  },
+  // Force rebuild
+  generateBuildId: async () => 'v3-' + Date.now()
 }
 
 module.exports = nextConfig
